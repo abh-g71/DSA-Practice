b7fa00fe9ddb72864877f709d3519c5fadca6753
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-01-27 08:06:42 +0530

    Time: 79 ms (46.81%) | Memory: 11.1 MB (28.02%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
 
     int solve(int start,int end,vector<vector<int>> &dp){
+        
+       
         if(start>=end){
             return 0;
         }
@@ -13,6 +15,23 @@ public:
             ans = min(ans,i+max(solve(start,i-1,dp),solve(i+1,end,dp)));
         }
         return dp[start][end] = ans;
+    }
+    int solve(int n){
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        for(int start = n ; start >=1;start--){
+            for(int end = start ; end<=n;end++){
+                if(start == end){
+                    continue;
+                }
+                 int ans = INT_MAX;
+                    for(int i = start ;i<=end;i++){
+                        ans = min(ans,i+max(dp[start][i-1],dp[i+1][end]));
+                    }
+                 dp[start][end] = ans;
+            }
+        }
+        return dp[1][n];
     }
     int getMoneyAmount(int n) {
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));

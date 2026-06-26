0533fa29386588b0af407560f6758b7728620b33
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-06-26 07:23:20 +0530

    Time: 4 ms (63.59%) | Memory: 19.3 MB (67.35%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+
+        for(int i = 0 ; i <  prerequisites.size() ; i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int>q;
+        for(int i = 0 ; i  < numCourses ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int count = 0;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            count++;
+
+            for(auto nei : adj[u]){
+                indegree[nei]--;
+                if(indegree[nei] == 0){
+                    q.push(nei);
+                }
+            }
+        }
+        return count == numCourses;
+    }
+};

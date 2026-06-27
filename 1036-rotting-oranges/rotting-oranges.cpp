0451f70aa9fcc1b5558e017910156e4b2317396b
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-06-27 09:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (97.50%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,65 +1,54 @@
 class Solution {
 public:
-
-    
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
+
+       
         queue<pair<int,int>>q;
         int fresh = 0;
-        
 
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0; j < m ; j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j});
-                    
-                }
+            for(int j = 0 ; j  < m ; j++){
                 if(grid[i][j] == 1){
                     fresh++;
                 }
+               else if(grid[i][j] == 2){
+                    
+                    q.push({i,j});
+                }
             }
         }
-
-
+        if(fresh == 0){
+            return 0;
+        }
+        int ans = 0;
+        int dr[] = {0,0,1,-1};
+        int dc[] = {-1,1,0,0};
         while(!q.empty() && fresh > 0){
             int size = q.size();
             
-            
+            while(size-- ){
+                auto [x,y] = q.front();
+                q.pop();
 
-            for(int i = 0 ;  i < size ; i++){
-                int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-                if(y+1 < m && grid[x][y+1] == 1){
-                    grid[x][y+1] = 2;
-                    fresh--;
-                    q.push({x,y+1});
-                }
-                if(y-1 >= 0 && grid[x][y-1] == 1){
-                    grid[x][y-1] = 2;
-                    fresh--;
-                    q.push({x,y-1});
-                }
-                if(x+1 < n && grid[x+1][y] == 1){
-                    grid[x+1][y] = 2;
-                    fresh--;
-                    q.push({x+1,y});
-                }
-                if(x-1 >= 0 && grid[x-1][y] == 1){
-                    grid[x-1][y] = 2;
-                    fresh--;
-                    q.push({x-1,y});
+                for(int i = 0 ; i < 4 ; i++){
+                    int nr = x + dr[i];
+                    int nc = y + dc[i];
+
+                    if(nc < m && nc >= 0 && nr < n && nr >= 0 && grid[nr][nc] == 1){
+                        fresh--;
+                        grid[nr][nc] = 2;
+                        q.push({nr,nc});
+                    }
                 }
             }
-            count++;
+            ans++;
+            
         }
-
-        if(fresh > 0){
+        if(fresh > 0 ){
             return -1;
         }
-        return count;
-        
+        return ans;
     }
 };

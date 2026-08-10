d2c95483272b343b532a5db8e177f535e73bf59f
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-08-10 20:18:56 +0530

    Time: 795 ms (26.34%) | Memory: 11.5 MB (37.27%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,37 +1,54 @@
 class Solution {
 public:
-int n , m;
 
-    bool solve(int i , int j ,vector<vector<char>>& board,string& word,int w){
-        if(w==word.size()){
-            return true;
-        }
-        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[w]){
-            return false;
+    bool solve(vector<vector<char>>& board, string word,int i , int j , int  k){
+        int n = board.size();
+        int m = board[0].size();
+        int o = word.size();
+
+        if(k==o){
+            return  true;
         }
         
+        if(i >= n || j >= m || i<0 || j<0 || board[i][j] != word[k] ){
+           return false;
+        } 
+
         char temp = board[i][j];
-        board[i][j] = '@';
+        board[i][j] = '#';
+
         bool found = 
-            solve(i+1,j,board, word,w+1) ||  solve(i,j-1,board, word,w+1) ||  solve(i,j+1,board, word,w+1) ||  solve(i-1,j,board, word,w+1);
 
-            board[i][j] = temp;
+           solve(board,word,i+1,j,k+1) ||
+            solve(board,word,i,j+1,k+1) ||
+             solve(board,word,i-1,j,k+1) ||
+              solve(board,word,i,j-1,k+1);
+        
+        board[i][j] = temp;
 
-            return found;
+        return found;
         
     }
     bool exist(vector<vector<char>>& board, string word) {
-         n = board.size();
-         m = board[0].size();
+        int n = board.size();
+        int m = board[0].size();
+        int o = word.size();
+       
        
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j =  0 ; j < m; j++){
-                if(solve(i,j,board,word,0)){
-                    return true;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0 ;  j < m ; j++){
+            
+                if(board[i][j] == word[0]){
+                     if(solve(board,word,i,j,0)){
+                        return true;
+                     }
                 }
+               
             }
+           
         }
+        
         return false;
     }
 };

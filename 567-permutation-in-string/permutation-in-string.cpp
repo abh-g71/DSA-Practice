dba7daf968dc641863552296662e0168ed4119da
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-09-23 12:18:24 +0530

    Time: 4 ms (52.77%) | Memory: 9.9 MB (70.45%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        if(n >m){
+            return false;
+        }
+        vector<int>a(26,0);
+        vector<int>b(26,0);
+        for(char c : s1){
+            a[c-'a']++;
+        }
+        for(int i = 0 ; i < n ; i++){
+            b[s2[i]-'a']++;
+        }
+        if(a==b){
+            return true;
+        }
+        for(int i = n ; i < m ; i++){
+            b[s2[i]-'a']++;
+            b[s2[i-n]-'a']--;
+            if(a==b){
+                return true;
+            }
+        }
+        return false;
+    }
+};

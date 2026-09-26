e96d5571ed560a5a9a39eae3fc32df4c016d152f
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-09-26 09:06:05 +0530

    Time: 82 ms (60.60%) | Memory: 48.2 MB (9.40%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -3,48 +3,41 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>>adj(n+1);
 
-        for(int i = 0 ; i < times.size(); i++){
-            int u = times[i][0];
-             int v = times[i][1];
-              int w = times[i][2];
+        for(auto time : times){
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
 
-              adj[u].push_back({v,w});
+            adj[u].push_back({v,w});
+            
         }
-
-        vector<int>time(n+1,1e9);
-        
-        time[k] = 0;
-
-        priority_queue<
-           pair<int,int>,
-           vector<pair<int,int>>,
-           greater<pair<int,int>>
-        >pq;
-
+        vector<int>dist(n+1,INT_MAX);
+        priority_queue<pair<int,int>,
+                        vector<pair<int,int>>,
+                        greater<pair<int,int>>>pq;
+        dist[k] = 0;
         pq.push({0,k});
-        
+        int time = 0;
 
         while(!pq.empty()){
-            auto [dis,node] = pq.top();
+            auto [t,node] = pq.top();
             pq.pop();
-           
-            if(dis > time[node]){
-                continue;
-            }
-            for(auto [adjnode,wt] : adj[node]){
-                if(wt + dis < time[adjnode]){
-                    time[adjnode] = wt+dis;
-                    pq.push({time[adjnode],adjnode});
+            if(t > dist[node]) continue;
+            
+
+            for(auto [nei,w] : adj[node]){
+                if(t+w < dist[nei]){
+                    dist[nei] = t+w;
+                    pq.push({dist[nei],nei});
                 }
             }
         }
-        int maxi = INT_MIN;
-        for(int i = 1 ; i <= n ; i++){
-            if(time[i] == 1e9){
+        for(int i = 1; i < n+1 ; i++){
+            if(dist[i] == INT_MAX){
                 return -1;
             }
-            maxi = max(time[i],maxi);
+            time = max(time,dist[i]);
         }
-        return maxi;
+        return time;
     }
 };
